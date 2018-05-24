acd31cbef1a641ed44fc3b1479e93c4d1890e32e
ChanJair <chan.jair@gmail.com>
2018-05-24 12:09:27 -0500

    Mi primer deployd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';     // Add this
+import { AboutComponent } from './about/about.component'; 
+import { LizComponent } from './liz/liz.component'; 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about/:id',
+    component: AboutComponent
+  },
+  {
+    path: 'liz',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,20 @@ export class HomeComponent implements OnInit {
   btnText2: string = 'Metele mas!';
   a: string = "a";
   goalText: string = 'My first life goal'; 
+  goals = [];
+
+  textoPrac: string = 'Haciendo prueba';
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+
   }
 
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
 }
